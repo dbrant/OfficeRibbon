310160751d0ef7cef3ff9266a8b60993631a35fd
Dmitry Brant <dbrant@wikimedia.org>
2017-08-29 23:40:27 -0400

    Make buildable for .NET 2.0, but set project defaults to 4.0.

--- RibbonDemo/MDIChild2.cs
+++ RibbonDemo/MDIChild2.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace RibbonDemo
 {
-   public partial class MDIChild2 : Form
+    public partial class MDIChild2 : Form
    {
       public MDIChild2()
       {
